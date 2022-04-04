f34ee8da93a6de1e9de39ae50b24b578de18bb28
vasanth8888 <vasanthveeda109@gmail.com>
2022-04-04 19:30:50 +0530

    github12

---
+++ src/test/java/org/git/Git12.java
@@ -0,0 +1,21 @@
+package org.git;
+
+public class Git12 {
+	
+	
+	private void hai1() {
+		// TODO Auto-generated method stub
+  System.out.println(1);
+	}
+	
+	private void hai2() {
+		// TODO Auto-generated method stub
+System.out.println(2);	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
